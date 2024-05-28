f9a6f4da97510ef541d50ee1e24774c1b1dded50
Pix4gunXd <Caio.pompei@gmail.com>
2024-05-28 20:38:59 -0300

    Menus Remake

--- Menus/MainMenu/Controls/Controls.gd
+++ Menus/MainMenu/Controls/Controls.gd
@@ -3,13 +3,6 @@ extends Control
 func _ready():
 	$TitleAnimation.play("Title")
 
-func _on_arena_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main/Arena/Arena.tscn")
-
-func _on_arena_mouse_entered():
-	$ButtonHover.play()
-
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Menus/MainMenu/menu.tscn")
 
