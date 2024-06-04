571a322ec6a47834572133160afbff7646517e56
Pix4gunXd <Caio.pompei@gmail.com>
2024-06-04 17:28:37 -0300

    Added music by: Caio Pompei Campos

---
+++ Sounds/audio_player.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer
+
+const bg_music = preload("res://Sounds/Try_Hit_Me__Menu_Theme.mp3")
+
+func _play_music(music: AudioStream, volume = 0.0):
+	if stream == music:
+		return
+	
+	stream = music
+	volume_db = volume
+	play()
+	
+func play_music_bg():
+	_play_music(bg_music)
