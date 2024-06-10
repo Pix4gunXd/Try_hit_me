357ea706cfc6e37ff0f998eb39dfa7872a369085
Pix4gunXd <Caio.pompei@gmail.com>
2024-06-10 01:37:34 -0300

    Added a background to pause_menu and a animation Hurt to Bobot

---
+++ Menus/Hud/game_over.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+# Criar funçao que identifique quando o jogo se encerra com a morte do jogador e ative a tela de GameOver dando opção de reiniciar ou sair
+# ATENÇÃO!!! A PONTUAÇÃO DO JOGADOR TAMBÉM DEVE APARECER NESTA TELA AO FINALIZAR
