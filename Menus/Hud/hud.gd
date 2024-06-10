192d137562bab84f6ccbb8a548dd5ac7e00215df
Eduardo Deeke <eduardohmdeeke@gmail.com>
2024-06-10 18:33:34 -0300

    Fixed a mistake

--- Menus/Hud/hud.gd
+++ Menus/Hud/hud.gd
@@ -5,16 +5,10 @@ var weapon1_image = preload("res://Weapons/Glock/glock_17_1.png")
 var weapon2_image = preload("res://Weapons/Rifle/Rifle_01.png")
 var weapon3_image = preload("res://Weapons/Shotgun/m1014_1.png")
 
-# Load the HUD scene
-var hud_scene = preload("res://Menus/Hud/hud.tscn")
-var hud_instance = null
 var weapon_image_texture_rect = null
 
 func _ready():
-	hud_instance = hud_scene.instantiate()
-	add_child(hud_instance)
-	
-	weapon_image_texture_rect = hud_instance.get_node("WeaponImage")
+	weapon_image_texture_rect = get_node("WeaponImage")
 
 func update_ammo(txt, max_ammo):
 	%Ammo.text = str(txt, " / ", max_ammo )
